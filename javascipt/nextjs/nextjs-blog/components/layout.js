2ce7a34c17d031e3c09e50aa4e1b65a64efcea1a
mahi5256 <m.p6092003@gmail.com>
2023-10-07 11:49:34 +0530

    changing dir

--- javascipt/nextjs/nextjs-blog/components/layout.js
+++ javascipt/nextjs/nextjs-blog/components/layout.js
@@ -1,3 +1,5 @@
+import styles from './layout.module.css';
+
 export default function Layout({ children }) {
-  return <div>{children}</div>;
+  return <div className={styles.container}>{children}</div>;
 }
--- javascipt/nextjs/nextjs-blog/pages/post/firstPost.jsx
+++ javascipt/nextjs/nextjs-blog/pages/post/firstPost.jsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import React from "react";
 import Layout from "../../components/layout";
+import styles from './firstPost.module.css'
 const firstPost = () => {
   return (
-    <Layout>
-      <h1>First Post</h1>
-      <h2>
+    <Layout >
+      
+      <h1 className={styles.heading1}>First Post</h1>
+      <h2 className={styles.heading2}>
         <Link href="/">Back to Home</Link>
       </h2>
     </Layout>
