f303d161067fde531d0992f043735c1596b3efb4
mp2003 <m.p6092003@gmail.com>
2024-02-07 23:44:25 +0530

    removing it from react revised

---
+++ projects/anime-vault/components/AnimeCard.tsx
@@ -0,0 +1,88 @@
+import Image from "next/image";
+import { delay, motion } from "framer-motion";
+import { MotionDiv } from "./MotionDiv";
+
+export interface AnimeProp {
+  id: string;
+  name: string;
+  image: {
+    original: string;
+  };
+  kind: string;
+  episodes: number;
+  episodes_aired: number;
+  score: string;
+}
+
+interface Prop {
+  anime: AnimeProp;
+  index: number;
+}
+
+const variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
+
+function AnimeCard({ anime, index }: Prop) {
+  return (
+    <MotionDiv
+      variants={variants}
+      initial="hidden"
+      animate="visible"
+      transition={{
+        delay: index * 0.25,
+        ease: "easeInOut",
+        duration: 0.5,
+      }}
+      className="max-w-sm rounded relative w-full"
+    >
+      <div className="relative w-full h-[37vh]">
+        <Image
+          src={`https://shikimori.one${anime.image.original}`}
+          alt={anime.name}
+          fill
+          className="rounded-xl"
+        />
+      </div>
+      <div className="py-4 flex flex-col gap-3">
+        <div className="flex justify-between items-center gap-1">
+          <h2 className="font-bold text-white text-xl line-clamp-1 w-full">
+            {anime.name}
+          </h2>
+          <div className="py-1 px-2 bg-[#161921] rounded-sm">
+            <p className="text-white text-sm font-bold capitalize">
+              {anime.kind}
+            </p>
+          </div>
+        </div>
+        <div className="flex gap-4 items-center">
+          <div className="flex flex-row gap-2 items-center">
+            <Image
+              src="./episodes.svg"
+              alt="episodes"
+              width={20}
+              height={20}
+              className="object-contain"
+            />
+            <p className="text-base text-white font-bold">
+              {anime.episodes || anime.episodes_aired}
+            </p>
+          </div>
+          <div className="flex flex-row gap-2 items-center">
+            <Image
+              src="./star.svg"
+              alt="star"
+              width={18}
+              height={18}
+              className="object-contain"
+            />
+            <p className="text-base font-bold text-[#FFAD49]">{anime.score}</p>
+          </div>
+        </div>
+      </div>
+    </MotionDiv>
+  );
+}
+
+export default AnimeCard;
---
+++ projects/anime-vault/components/LoadMore.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { fetchAnime } from "@/app/action";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
+
+let page = 2;
+
+export type AnimeCard = JSX.Element;
+
+function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<JSX.Element[]>([]);
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page).then((res) => {
+        setData((prevData) => [...prevData, ...res]);
+        page++;
+      });
+    }
+  }, [inView, page]);
+
+  // console.log(fetchAnime(3).then((data) => console.log(data)));
+  return (
+    <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data}
+      </section>
+      <section className="flex justify-center items-center w-full">
+        <div ref={ref}>
+          <Image
+            src="./spinner.svg"
+            alt="spinner"
+            width={56}
+            height={56}
+            className="object-contain"
+          />
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default LoadMore;
